af3b7b2fef0e17903892a9c288f2dcd7dc0f4635
prasannauma <uprasanna.68@gmail.com>
2023-01-12 11:43:28 +0530

    this code is to create multiple servers..

--- create-server.sh
+++ create-server.sh
@@ -2,8 +2,8 @@
 #!/bin/bash
 
 ##### Change these values ###
-ZONE_ID="Z0366464237Z7LZLZPKFA"
-DOMAIN="devopsb70.online"
+ZONE_ID="Z090620915JFWV927GK62"
+DOMAIN="umadevops.online"
 SG_NAME="allow-all"
 env=dev
 #############################
